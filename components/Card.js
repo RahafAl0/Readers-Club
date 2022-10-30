32d5457fc4fb1f349562786c982e03747d918748
RahafAl0 <ra.al9760@gmail.com>
2022-10-30 22:45:26 +0300

    add card component

---
+++ components/Card.js
@@ -0,0 +1,16 @@
+import './Card.module.css';
+
+
+const Card = (props) => {
+  return (
+    <div>
+      <img src={props.bookCover} width="160px"/>
+      <p>{props.bookName}</p>
+      <p>by {props.authorName}</p>
+      <p>Progress {props.progress}%</p>
+      <p>Rating {props.rating}</p>
+    </div>
+  )
+}
+
+export default Card;
