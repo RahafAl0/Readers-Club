ab6782be71ed52c9e805513dcea441330da983d5
RahafAl0 <ra.al9760@gmail.com>
2022-11-02 23:59:27 +0300

    used card and button components

--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,6 @@ import Button from '../components/Buttons'
 
 
 export default function Home() {
-  const randomName = faker.name.fullName(); // Rowan Nikolaus
-  const bookName = faker.animal.cat();
-  const bookCover = faker.image.cats();
 
   const generateProgress = () => {
     return Math.floor(Math.random() * 100);
@@ -25,15 +22,15 @@ export default function Home() {
         <div className={styles.grid}>
           <a className={styles.card}>
             <h2><Card 
-              bookCover={bookCover} 
-              bookName={randomName} 
-              authorName={bookName} 
+              bookCover={faker.image.cats()} 
+              bookName={faker.animal.cat()} 
+              authorName={faker.name.fullName()} 
               progress={generateProgress()} 
               rating={rating()} 
               />
-              <Button text='Add to'/>
-              <Button text='Add review'/>
-              <Button text='Add book'/>
+              <Button bgColor='aqua' text='Add to'/>
+              <Button bgColor='aqua' text='Add review'/>
+              <Button bgColor='blueviolet' text='Add book' />
 
             </h2>
           </a>
