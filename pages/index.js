b162400a9b095f0cee6f69645a0a71fbf072f172
RahafAl0 <ra.al9760@gmail.com>
2022-11-03 20:54:52 +0300

    added UserProgress component

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Card from '../components/Card'
 import styles from '../styles/Home.module.css'
 import { faker } from '@faker-js/faker';
 import Button from '../components/Buttons'
+import UserProgress from '../components/UserProgress';
 
 
 export default function Home() {
@@ -21,16 +22,37 @@ export default function Home() {
 
         <div className={styles.grid}>
           <a className={styles.card}>
-            <h2><Card 
-              bookCover={faker.image.cats()} 
-              bookName={faker.animal.cat()} 
-              authorName={faker.name.fullName()} 
-              progress={generateProgress()} 
-              rating={rating()} 
+          
+            <h2>
+              <Card 
+                bookCover={faker.image.cats()} 
+                bookName={faker.animal.cat()} 
+                authorName={faker.name.fullName()} 
+                progress={generateProgress()} 
+                rating={rating()} 
               />
+
               <Button bgColor='aqua' text='Add to'/>
               <Button bgColor='aqua' text='Add review'/>
-              <Button bgColor='blueviolet' text='Add book' />
+              <Button bgColor='blueviolet' fontColor='aqua' text='Add book' />
+            
+          
+<           div className={styles.grid}>
+                <UserProgress className={styles.card} bgColor='blueviolet' text='All Book' status={Math.floor(Math.random() * 100)} /> 
+            </div>
+
+            <div className={styles.grid}> 
+                <UserProgress className={styles.card} bgColor='blueviolet' text='read' status={Math.floor(Math.random() * 100)} /> 
+            </div>
+
+            <div className={styles.grid}>
+              <UserProgress className={styles.card} bgColor='blueviolet' text='Currently Reading' status={Math.floor(Math.random() * 100)} /> 
+            </div>
+            <div className={styles.grid}>
+      
+              <UserProgress className={styles.card} bgColor='blueviolet' text='Want To Read' status={Math.floor(Math.random() * 100)} />
+          
+            </div>
 
             </h2>
           </a>
@@ -38,4 +60,6 @@ export default function Home() {
       </main>
     </div>
   )
+  
 }
+
