3265f3285c8f071d50875ab7f65bd2b18cc12212
RahafAl0 <ra.al9760@gmail.com>
2022-11-03 19:55:36 +0300

    added padding and font color

--- components/Buttons.js
+++ components/Buttons.js
@@ -2,7 +2,7 @@
 
 const Button = (props) => {
   return ( 
-    <button style={{background:props.bgColor, color:props.fontColor}} >{props.text}</button> 
+    <button style={{background:props.bgColor, color:props.fontColor, padding: '16px'}} size='lg'>{props.text}</button> 
   );
 };
 
