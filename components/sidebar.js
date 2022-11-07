1d9efc4029e337fc0fe9a464bf2cb26c235bc81e
RahafAl0 <ra.al9760@gmail.com>
2022-11-07 18:25:17 +0300

    done editing sidebar

--- components/sidebar.js
+++ components/sidebar.js
@@ -8,42 +8,54 @@ const Sidebar = () => {
   return (
     
     <div>
-      <div style={{width: '400px', height:'100vh', backgroundColor:'blueviolet' ,display:'flex', flexDirection:'column', alignItems:'', justifyContent:'center'}}>
-            <Image style={{display:'flex', margin: 'auto'}}  width='150px' height='100' src={faker.image.cats()} roundedCircle/>
-       
+      <div style={{width: '300px', height:'100vh', backgroundColor:'blueviolet' ,display:'flex', flexDirection:'column', alignItems:'', justifyContent:'center'}}>
+            <Image style={{display:'flex', margin: 'auto', width:170, height:170}} 
+                   src="https://loremflickr.com/640/480/cats" roundedCircle
+            />
         <div style={{color:'white', margin:'auto'}}>
           <h4>Rahaf Almusleh</h4>
           </div>
-          <div style={{display:'flex', gap:'10px', margin:'auto'}}>
+          <div style={{display:'flex', gap:'5px', margin:'auto'}}>
             <div>
-              <Card class="p-2 border" style={{ width: '10rem', height: '10rem', backgroundColor:'darkorchid', color:'white' }}>
+              <Card class="p-2 border" style={{ width: '7rem', height: '7rem', backgroundColor:'darkorchid', color:'white' }}>
               <Card.Body>
                 <Card.Text>
-                  All Book {Math.floor(Math.random() * 100)}
+                {Math.floor(Math.random() * 100)}
+                </Card.Text>
+                <Card.Text>
+                  All Book
                 </Card.Text>
               </Card.Body>
             </Card>
-            <Card class="p-2 border" style={{ width: '10rem', height: '10rem', backgroundColor:'darkorchid', color:'white' }}>
+            <Card class="p-2 border" style={{ width: '7rem', height: '7rem', backgroundColor:'darkorchid', color:'white' }}>
               <Card.Body>
-                <Card.Text>
-                  Read {Math.floor(Math.random() * 100)}
+              <Card.Text>
+                {Math.floor(Math.random() * 100)}
                 </Card.Text>
-                
+                <Card.Text>
+                  Read 
+                </Card.Text>
               </Card.Body>
             </Card>
           </div>
           <div>
-            <Card class="p-2 border" style={{ width: '10rem', height: '10rem', backgroundColor:'darkorchid', color:'white' }}>
+            <Card class="p-2 border" style={{ width: '7rem', height: '7rem', backgroundColor:'darkorchid', color:'white' }}>
               <Card.Body>
+              <Card.Text>
+                {Math.floor(Math.random() * 100)}
+                </Card.Text>
                 <Card.Text>
-                  Currently Reading {Math.floor(Math.random() * 100)}
+                  Currently Reading 
                 </Card.Text>
               </Card.Body>
             </Card>
-            <Card class="p-2 border" style={{ width: '10rem', height: '10rem', backgroundColor:'darkorchid', color:'white' }}>
+            <Card class="p-2 border" style={{ width: '7rem', height: '7rem', backgroundColor:'darkorchid', color:'white' }}>
               <Card.Body>
+              <Card.Text>
+                {Math.floor(Math.random() * 100)}
+                </Card.Text>
                 <Card.Text>
-                  Want to Read {Math.floor(Math.random() * 100)}
+                  Want to Read 
                 </Card.Text>
               </Card.Body>
             </Card>
