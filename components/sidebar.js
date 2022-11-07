fabcceecf931908630ea0f218ff5e534e9bbc18a
RahafAl0 <ra.al9760@gmail.com>
2022-11-07 10:33:57 +0300

    added propfile pic and name

--- components/sidebar.js
+++ components/sidebar.js
@@ -1,52 +1,61 @@
 import Card from 'react-bootstrap/Card';
 import { faker } from '@faker-js/faker';
 import CustomButton from './Button';
-
+import Image from "react-bootstrap/Image";
 
 const Sidebar = () => {
   return (
     <div>
-      <div  style={{width: '400px', height:'100vh', backgroundColor:'blueviolet' ,display:'flex', gap:'10px', justifyContent:'center'}}>
-        
-        <div >
-        <Card class="p-2 border" style={{ width: '10rem', height: '10rem', backgroundColor:'darkorchid', color:'white' }}>
-        <Card.Body>
-          <Card.Text>
-            All Book {Math.floor(Math.random() * 100)}
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <Card class="p-2 border" style={{ width: '10rem', height: '10rem', backgroundColor:'darkorchid', color:'white' }}>
-        <Card.Body>
-          <Card.Text>
-            Read {Math.floor(Math.random() * 100)}
-          </Card.Text>
-          
-        </Card.Body>
-      </Card>
-      </div>
-      <div>
-      <Card class="p-2 border" style={{ width: '10rem', height: '10rem', backgroundColor:'darkorchid', color:'white' }}>
-        <Card.Body>
-          <Card.Text>
-            Currently Reading {Math.floor(Math.random() * 100)}
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <Card class="p-2 border" style={{ width: '10rem', height: '10rem', backgroundColor:'darkorchid', color:'white' }}>
-        <Card.Body>
-          <Card.Text>
-            Want to Read {Math.floor(Math.random() * 100)}
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <CustomButton display='flex' variant='primary'>Add Shelf</CustomButton> 
-        <CustomButton  variant='primary'>Add Book</CustomButton>
-      </div>
-       
-
+      <div style={{width: '400px', height:'100vh', backgroundColor:'blueviolet' ,display:'flex', flexDirection:'column', alignItems:'', justifyContent:'center'}}>
+        <div style={{ display: "flex", width: '700', padding: '30' }}> 
+          <Image src={faker.image.cats()} roundedCircle/>
+        </div>
+        <div style={{color:'white'}}>
+          <h4>Rahaf Almusleh</h4>
+          </div>
+          <div style={{display:'flex', gap:'10px'}}>
+            <div>
+              <Card class="p-2 border" style={{ width: '10rem', height: '10rem', backgroundColor:'darkorchid', color:'white' }}>
+              <Card.Body>
+                <Card.Text>
+                  All Book {Math.floor(Math.random() * 100)}
+                </Card.Text>
+              </Card.Body>
+            </Card>
+            <Card class="p-2 border" style={{ width: '10rem', height: '10rem', backgroundColor:'darkorchid', color:'white' }}>
+              <Card.Body>
+                <Card.Text>
+                  Read {Math.floor(Math.random() * 100)}
+                </Card.Text>
+                
+              </Card.Body>
+            </Card>
+          </div>
+          <div>
+            <Card class="p-2 border" style={{ width: '10rem', height: '10rem', backgroundColor:'darkorchid', color:'white' }}>
+              <Card.Body>
+                <Card.Text>
+                  Currently Reading {Math.floor(Math.random() * 100)}
+                </Card.Text>
+              </Card.Body>
+            </Card>
+            <Card class="p-2 border" style={{ width: '10rem', height: '10rem', backgroundColor:'darkorchid', color:'white' }}>
+              <Card.Body>
+                <Card.Text>
+                  Want to Read {Math.floor(Math.random() * 100)}
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </div>
+      
+        </div>
+        <div style={{display:'flex'}}>
+          <CustomButton display='flex' variant='primary'>Add Shelf</CustomButton> 
+          <CustomButton  variant='primary'>Add Book</CustomButton>
+        </div> 
       </div>
     </div>
+    
   );
 }
 
