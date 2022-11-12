9c5c3b40b59f59e49a5a2d90feac404ffddb6548
RahafAl0 <ra.al9760@gmail.com>
2022-11-12 23:59:14 +0300

    edit style in book detail

--- components/BookReview/BookDetail.js
+++ components/BookReview/BookDetail.js
@@ -3,16 +3,25 @@ import { faker } from '@faker-js/faker';
 
 const BookDetail = () => {
   return(
-    <Card class="p-2 border" style={{ width: '50rem',height:'30rem', margin:'auto' }}>
-      <Card.Img class="rounded  d-block" src={faker.image.cats()} width='200px' height='300px'  />
-      <Card.Body>
-      <Card.Title>{faker.animal.cat()}</Card.Title>
-      <Card.Text>
-          Some quick example
-      </Card.Text>
+    <div style={{margin: 'right'}}>
+      <Card class="p-2 border" style={{ width: '50rem',height:'30rem', margin:'auto' }}>
+        <Card.Img class="rounded  d-block" src={faker.image.cats()} width='200px' height='300px'  />
+        <Card.Body>
+        
+        <Card.Title style={{textIndent:'10cm'}}>
+          {faker.name.fullName()}
           
-        </Card.Body>
-    </Card>
+          {faker.animal.cat()}
+         
+
+        </Card.Title>
+        <Card.Text style={{textIndent:''}}>
+            Some quick example text to build on the card title 
+            and make up the bulk of the card's content.
+        </Card.Text >
+          </Card.Body>
+      </Card>
+    </div>
   )
 }
 
