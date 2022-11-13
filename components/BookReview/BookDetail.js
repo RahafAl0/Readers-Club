3af65892007e06ab598bce70a0c22877c7cab6a7
RahafAl0 <ra.al9760@gmail.com>
2022-11-13 14:03:48 +0300

    remove bootstrapReact and use bootstrap

--- components/BookReview/BookDetail.js
+++ components/BookReview/BookDetail.js
@@ -1,26 +1,25 @@
-import Card from 'react-bootstrap/Card';
 import { faker } from '@faker-js/faker';
 
 const BookDetail = () => {
   return(
-    <div style={{margin: 'right'}}>
-      <Card class="p-2 border" style={{ width: '50rem',height:'30rem', margin:'auto' }}>
-        <Card.Img class="rounded  d-block" src={faker.image.cats()} width='200px' height='300px'  />
-        <Card.Body>
-        
-        <Card.Title style={{textIndent:'10cm'}}>
-          {faker.name.fullName()}
-          
-          {faker.animal.cat()}
-         
-
-        </Card.Title>
-        <Card.Text style={{textIndent:''}}>
-            Some quick example text to build on the card title 
-            and make up the bulk of the card's content.
-        </Card.Text >
-          </Card.Body>
-      </Card>
+    <div className="card border mb-3 w-700px h-120px m-auto">
+      <div class="row g-0">
+        <div class="col-md-4">
+          <img src={faker.image.cats()} class="img-fluid rounded-start" alt="..." style={{height:'450px'}}/>
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+          <p className="fw-bold fs-1">{faker.name.fullName()}</p>
+          <p className="fs-1 fw-bold">{faker.name.fullName()}</p>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
+            This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
+            This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
+            This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
+            This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
