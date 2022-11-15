ee035a59afbcf58dbeef852aa7f0092646521543
RahafAl0 <ra.al9760@gmail.com>
2022-11-15 13:36:14 +0300

    done editing profile picture window

--- components/BookReview/ProfilePicture.js
+++ components/BookReview/ProfilePicture.js
@@ -1,19 +1,26 @@
 const ProfilePicture = () => {
     return(
-        <div className="card border border-primary w-300px h-350px m-auto">
-          <div class="card-body">
-            <h5 class="text-center ">Profile Picture</h5>
-            <p class="text-center">Add a nice photo of yourself for your profile.</p>
-            <div className="p-10">
+        <div className="card border border-primary w-300px h-400px m-auto">
+          <button type="button" className="btn-close p-5" aria-label="Close"></button>
+          <div className="card-body">
+            <h5 className="text-center ">Profile Picture</h5>
+            <p className="text-center">Add a nice photo of yourself for your profile.</p>
+            <div className="p-5">
               <img className="rounded-circle mx-auto d-block "
                   src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(21).webp" alt="avatar" width="100"
                   height="100" />
             </div>
+            <div class="input-group mb-2">
+          <input type="text" class="form-control" placeholder="no file selected" aria-label="Recipient's username" aria-describedby="basic-addon2"/>
+          <div class="input-group-append">
+            <button className="btn btn-dark" type="button">Browse</button>
+          </div>
         </div>
-        <div class="mb-3">
-          <input className="form-control" type="file" id="formFile"/>
-          
+        <div className="text-center p-2">
+          <button className="btn btn-primary" type="button">Save</button>
         </div>
+        </div>
+       
       </div>
     )
   }
