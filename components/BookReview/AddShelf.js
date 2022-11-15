f2078478e33d9f0f50853c19b54192ebf0947999
RahafAl0 <ra.al9760@gmail.com>
2022-11-15 13:35:20 +0300

    done editing add shelf window

--- components/BookReview/AddShelf.js
+++ components/BookReview/AddShelf.js
@@ -1,11 +1,12 @@
 const AddShelf = () => {
   return(
-    <div class="m-auto border border-primary w-500px h-200px">
-      
-        <h5 className="p-10 text-center">Add Shelf</h5>
+    <div class="m-auto border border-primary w-500px h-250px">
+        <button type="button" className="btn-close p-5" aria-label="Close"></button>
+        <h5 className="p-5 text-center">Add Shelf</h5>
         <input className=" m-auto w-400px form-control" type="text" placeholder="Add shelf" aria-label="default input example"/>
-       
-        <button class="btn btn-primary me-md-2" type="button">Button</button>
+      <div className="p-5">
+        <button class="btn btn-primary me-md-10 float-end" type="button">Add</button>
+      </div>
       </div>
   )
 }
