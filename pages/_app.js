06cc190a310f36c1c1956c3efb01a30a84ea0450
RahafAl0 <ra.al9760@gmail.com>
2022-11-22 13:40:22 +0300

    add searchbar and sidebar for all pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
 import '../styles/globals.css'
+import Searchbar from '../components/BookPages/Searchbar'
+import Sidebar from '../components/Sidebar'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="d-flex flex-column flex-root">
+      <div className='page d-flex flex-row flex-column-fluid'>
+        <div className='wrapper d-flex flex-column flex-row-fluid'>
+          <div className='header'>
+            <Searchbar/>          </div>
+          <div className='d-flex flex-column flex-column-fluid'>
+            <Component {...pageProps} />
+          </div>
+          
+        </div>
+        <div className='sidebar bg-info'>
+            <Sidebar/>          </div>
+      </div>
+    </div>
+  )
 }
 
 export default MyApp
