3d1f9dd1377c38cfa58e66f2a914768355d7224c
RahafAl0 <ra.al9760@gmail.com>
2022-11-24 00:08:03 +0300

    edit style

--- components/Card.js
+++ components/Card.js
@@ -2,15 +2,15 @@ import { faker } from '@faker-js/faker';
 
 const BookCard = () => {
   return(
-    <div className="card border w-200px h-300px m-auto">
-      <div className="p-3">
-        <img className=" m-auto d-block w-100px h-150px"
-             src={faker.image.cats()} alt="avatar" width="100"
+    <div className="card border w-175px h-275px m-auto">
+      <div>
+        <img className=" m-auto d-block w-75px h-125px"
+             src={faker.image.cats()} alt="avatar" width="75"
              height="100" />
       </div>
       <div className="card-body">
-        <h5 className="text">{faker.animal.cat()}</h5>
-        <p className="text">{faker.name.fullName()}</p>
+        <h5 className="text">1984</h5>
+        <p className="text">By George Orewell</p>
         <div class="progress">
           <div class="progress-bar w-75" role="progressbar" aria-label="Basic example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
           25%
