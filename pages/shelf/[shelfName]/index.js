f48dcffb2b85a3bb1e9529e7eee8d28b04907db6
RahafAl0 <ra.al9760@gmail.com>
2022-11-28 09:39:49 +0300

    edit shelf screen to open any shelf

---
+++ pages/shelf/[shelfName]/index.js
@@ -0,0 +1,29 @@
+import ShelfScreen from "../../../components/ShelfNamePages/ShelfScreen";
+import { useRouter } from "next/router";
+
+export default function ShelfName (props) {
+  const router = useRouter()
+  function capitalize(str){ 
+    return  str[0].toUpperCase() + str.substr(1);
+  }
+  console.log(router.query.shelfName)
+  if(props.shelf == 'Not found') return <h1>Page Not Found!</h1> 
+  return (
+    
+      <ShelfScreen shelfName={capitalize(props.shelf)}/>
+
+    
+  )
+}
+
+export const getServerSideProps = async (context) => {
+  const {query} = context;
+
+  const shelfs = ['read', 'currently-reading', 'my-shelf', 'to-read']
+  const shelfIndex = shelfs.findIndex((item) => {
+    return item == query.shelfName.toLowerCase()
+  })
+  const shelf = shelfIndex == -1 ? null : query.shelfName;
+
+  return {props: {shelf: shelf ? shelf.replace("-", " ") : 'Not found'}}
+}
