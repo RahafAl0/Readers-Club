36ebfc4aa15e37b697f8dee6b9b005f5d4799ce8
RahafAl0 <ra.al9760@gmail.com>
2022-12-07 22:53:06 +0300

    remove (view more) button in shelf screen

--- components/ShelfNamePages/ShelfScreen.js
+++ components/ShelfNamePages/ShelfScreen.js
@@ -9,7 +9,6 @@ const ShelfScreen = (props) => {
           <div className='mb-10'>
           <div className='d-flex justify-content-between align-items-center mb-7'>
             <h2 className='fw-bolder text-dark fs-2 mb-0'>{props.shelfName}</h2>
-            <a href='#' className='btn btn-light-primary btn-sm fw-bolder'>View more</a>
           </div>
           <div className='row g-5 g-xxl-8 m-4'>
             <BookCard/>
