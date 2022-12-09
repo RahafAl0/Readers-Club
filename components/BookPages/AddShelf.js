376a6627f049a3832740fdb2f3088f4db6d9ee5a
RahafAl0 <ra.al9760@gmail.com>
2022-12-09 21:55:38 +0300

    refactor add new book into dialog and add commet to old design

--- components/BookPages/AddShelf.js
+++ components/BookPages/AddShelf.js
@@ -1,13 +1,50 @@
-const AddShelf = () => {
-  return(
-    <div class="m-auto w-400px h-225px">
-      <h5 className="p-5 text-center">Add Shelf</h5>
-      <input className=" m-auto w-375px form-control" type="text" placeholder="Add shelf" aria-label="default input example"/>
-      <div className="p-5">
-        <button class="btn btn-primary me-md-10 float-end" type="button">Add</button>
-      </div>
-    </div>
-  )
-}
+import Dialog from "../Dialog";
+
+const AddShelf = ({ handleClose, show }) => {
+  return (
+    <Dialog
+      show={show}
+      handleClose={handleClose}
+      header={
+        <>
+          <h5 className="p-5 text-center">Add Shelf</h5>
+        </>
+      }
+      body={
+        <>
+          <input
+            className=" m-auto w-375px form-control"
+            type="text"
+            placeholder="Add shelf"
+            aria-label="default input example"
+          />
+        </>
+      }
+      footer={
+        <>
+          <button class="btn btn-primary me-md-10 float-end" type="button">
+            Add
+          </button>
+        </>
+      }
+    />
+  );
+  // return (
+  //   <div class="m-auto w-400px h-225px">
+  //     <h5 className="p-5 text-center">Add Shelf</h5>
+  //     <input
+  //       className=" m-auto w-375px form-control"
+  //       type="text"
+  //       placeholder="Add shelf"
+  //       aria-label="default input example"
+  //     />
+  //     <div className="p-5">
+  //       <button class="btn btn-primary me-md-10 float-end" type="button">
+  //         Add
+  //       </button>
+  //     </div>
+  //   </div>
+  // );
+};
 
 export default AddShelf;
