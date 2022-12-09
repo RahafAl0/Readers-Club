23f0b08b11dee9d2381f0a3bb0f44e717d4154d1
RahafAl0 <ra.al9760@gmail.com>
2022-12-09 10:11:47 +0300

    remove import button

--- components/Dialog.js
+++ components/Dialog.js
@@ -1,12 +1,13 @@
-import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
 function Dialog({handleClose, show, content}) {
   return (
       <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
         
+        </Modal.Header>
         <Modal.Body>{content}</Modal.Body>
-      
+          
       </Modal>
     
   );
