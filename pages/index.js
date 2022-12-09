23b866b4520effb34e63c37b9b99e96dd891393e
RahafAl0 <ra.al9760@gmail.com>
2022-12-10 00:52:48 +0300

    refactor

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,22 @@
-import BookShelfRow from '../components/BookShelfRow'
+import BookShelfRow from "../components/BookShelfRow";
 
 export default function Home() {
   return (
     <div>
-      {[{title: "Currently Reading", shelfUrl: "currently-reading"},{title: "Read", shelfUrl: "read"},{title: "To Read", shelfUrl: "to-read"},{title: "Want to read", shelfUrl: "want-to-read"}].map((val, index) => {
-          return(<BookShelfRow title={val.title} shelfUrl={val.shelfUrl}/>)
-      })}        
+      {[
+        { title: "Currently Reading", shelfUrl: "currently-reading" },
+        { title: "Read", shelfUrl: "read" },
+        { title: "To Read", shelfUrl: "to-read" },
+        { title: "Want to read", shelfUrl: "want-to-read" },
+      ].map((val, index) => {
+        return (
+          <BookShelfRow
+            key={val.title}
+            title={val.title}
+            shelfUrl={val.shelfUrl}
+          />
+        );
+      })}
     </div>
-  )
-  
+  );
 }
-
