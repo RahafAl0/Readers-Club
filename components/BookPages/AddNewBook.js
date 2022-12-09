ac64c90a075388470fb3b89e387292d40a5e3d75
RahafAl0 <ra.al9760@gmail.com>
2022-12-09 21:51:54 +0300

    refactor add new book into dialog

--- components/BookPages/AddNewBook.js
+++ components/BookPages/AddNewBook.js
@@ -1,30 +1,64 @@
 import BookCard from "../Card";
+import Dialog from "../Dialog";
 
-const AddNewBook = () => {
-  return(
-    <div>
-
-      <div className="card m-auto justify-content w-1000px h-600px border border-primary ">
-        <div className="card-body"> 
-          <div className='justify-content-between'>
+const AddNewBook = ({ handleClose, show }) => {
+  return (
+    <Dialog
+      show={show}
+      handleClose={handleClose}
+      header={
+        <>
+          <div className="justify-content-between">
             <h5 className="card-header p-2 m-2">Add book</h5>
-            <div className='m-10'>
-              <input class="form-control me-2" type="search" placeholder="Goerge Orewell" aria-label="Search"/>
+            <div className="m-10">
+              <input
+                class="form-control me-2"
+                type="search"
+                placeholder="Goerge Orewell"
+                aria-label="Search"
+              />
             </div>
           </div>
-          <div className='row m-2 p-2'>
-            <BookCard/>
-            <BookCard/>
-            <BookCard/>
-            <BookCard/>
+        </>
+      }
+      body={
+        <>
+          <div className="row m-2 p-2">
+            <BookCard />
+            <BookCard />
+            <BookCard />
+            <BookCard />
           </div>
-          <div className='card-footer m-10'>
-
+        </>
+      }
+    />
+  );
+  return (
+    <div>
+      <div className="card m-auto justify-content w-1000px h-600px border border-primary ">
+        <div className="card-body">
+          <div className="justify-content-between">
+            <h5 className="card-header p-2 m-2">Add book</h5>
+            <div className="m-10">
+              <input
+                class="form-control me-2"
+                type="search"
+                placeholder="Goerge Orewell"
+                aria-label="Search"
+              />
+            </div>
           </div>
+          <div className="row m-2 p-2">
+            <BookCard />
+            <BookCard />
+            <BookCard />
+            <BookCard />
+          </div>
+          <div className="card-footer m-10"></div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default AddNewBook;
