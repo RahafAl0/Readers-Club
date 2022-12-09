cb0ae4d226990211afc72cf1101d8070c867e2cb
RahafAl0 <ra.al9760@gmail.com>
2022-12-10 00:40:34 +0300

    edit calss to className

--- components/BookPages/AddBookToShelf.js
+++ components/BookPages/AddBookToShelf.js
@@ -1,14 +1,14 @@
-const AddBookToShelf = () => {
+const AddBookToShelf = ({handleClose}) => {
   return(
     <div>
       <div className="card m-auto w-500px border border-primary ">
         <div className="card-body"> 
           <div className='justify-content-between'>
             <h5 className="card-header p-2 m-2">Add Book</h5>
-            <div class="input-group rounded">
+            <div className="input-group rounded">
               <input type="search" className="form-control rounded" placeholder="Goerge Orewell" aria-label="Search" aria-describedby="search-addon" />
-              <span class="input-group-text border-0" id="search-addon">
-                <i class="fas fa-search"></i>
+              <span className="input-group-text border-0" id="search-addon">
+                <i className="fas fa-search"></i>
               </span>
             </div>
             <div className="m-10">
@@ -22,8 +22,8 @@ const AddBookToShelf = () => {
               <button type="button" className="m-2 border border-success btn btn-outline-success">Shelf4</button>
             </div>
             <div className="card-footer m-2">
-              <button class="btn btn-dark me-md-3 float-end" type="button">Cancel</button>
-              <button class="btn btn-primary me-md-3 float-end" type="button">Add</button>
+              <button className="btn btn-dark me-md-3 float-end" type="button" onClick={handleClose}>Cancel</button>
+              <button className="btn btn-primary me-md-3 float-end" type="button">Add</button>
             </div>
           </div>
         </div>
