d5ff28927ad483b4069e904ebb3e0232405a02fd
RahafAl0 <ra.al9760@gmail.com>
2022-12-10 00:44:47 +0300

    edit class to className

--- components/BookPages/BookDetail.js
+++ components/BookPages/BookDetail.js
@@ -3,12 +3,12 @@ import { faker } from '@faker-js/faker';
 const BookDetail = () => {
   return(
     <div className="card border mb-3 w-700px h-120px m-auto">
-      <div class="row g-0">
-        <div class="col-md-4">
+      <div className="row g-0">
+        <div className="col-md-4">
           <img src={faker.image.cats()} class="img-fluid rounded-start" alt="..." style={{height:'450px'}}/>
         </div>
-        <div class="col-md-8">
-          <div class="card-body">
+        <div className="col-md-8">
+          <div className="card-body">
           <p className="fw-bold fs-1">{faker.name.fullName()}</p>
           <p className="fs-1 fw-bold">{faker.name.fullName()}</p>
             <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
