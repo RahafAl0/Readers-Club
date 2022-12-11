5abc9e59ae73f804fae13bc454c11a5eab9d15fe
RahafAl0 <ra.al9760@gmail.com>
2022-12-11 23:50:54 +0300

    add useState to make user add new book to any shelf

--- components/BookPages/AddNewBook.js
+++ components/BookPages/AddNewBook.js
@@ -1,7 +1,19 @@
 import AddBookCard from "../AddBookCard";
 import Dialog from "../Dialog";
+import { useState, useEffect } from "react";
 
 const AddNewBook = ({ handleClose, show }) => {
+  const [addBookToShelf, setAddBookToShelf] = useState(false);
+  const [bookDeatails, setBookDetails] = useState({});
+  useEffect(() => {
+
+  }, [])
+
+  function handleAddBookToShelf(book) {
+    setAddBookToShelf(true);
+    setBookDetails(book);
+  }
+
   return (
     <Dialog
       show={show}
@@ -10,6 +22,7 @@ const AddNewBook = ({ handleClose, show }) => {
         <>
           <div className="justify-content-between">
             <h5 className="card-header p-2 m-2">Add book</h5>
+            
             <div className="m-10">
               <input
                 className="form-control me-2"
@@ -22,18 +35,92 @@ const AddNewBook = ({ handleClose, show }) => {
         </>
       }
       body={
-        <>
-          <div className="row m-2 p-2">
-            <AddBookCard title='1984' url='/book/1984' author='Goerge Orewell'/>
-            <AddBookCard title='1984' url='/book/1984' author='Goerge Orewell'/>
-            <AddBookCard title='1984' url='/book/1984' author='Goerge Orewell'/>
-            <AddBookCard title='1984' url='/book/1984' author='Goerge Orewell'/>
-          </div>
-        </>
+        addBookToShelf ? (
+          <>
+            <div className="m-10">
+              <h5>1984</h5>
+              <p>By Goerge Orewell</p>
+            </div>
+            <div className=" h-50px">
+              <button
+                type="button"
+                className="m-2 border border-success btn btn-outline-success"
+              >
+                Shelf1
+              </button>
+              <button
+                type="button"
+                className="m-2 border border-success btn btn-outline-success"
+              >
+                Shelf2
+              </button>
+              <button
+                type="button"
+                className="m-2 border border-success btn btn-outline-success"
+              >
+                Shelf3
+              </button>
+              <button
+                type="button"
+                className="m-2 border border-success btn btn-outline-success"
+              >
+                Shelf4
+              </button>
+            </div>
+          </> 
+
+        ) : (
+          <>
+            <div className="row m-2 p-2">
+              <AddBookCard
+                title="1984"
+                url="/book/1984"
+                author="Goerge Orewell"
+                handleAddBookToShelf={handleAddBookToShelf}
+              />
+              <AddBookCard
+                title="1984"
+                url="/book/1984"
+                author="Goerge Orewell"
+                handleAddBookToShelf={handleAddBookToShelf}
+              />
+              <AddBookCard
+                title="1984"
+                url="/book/1984"
+                author="Goerge Orewell"
+                handleAddBookToShelf={handleAddBookToShelf}
+              />
+              <AddBookCard
+                title="1984"
+                url="/book/1984"
+                author="Goerge Orewell"
+                handleAddBookToShelf={handleAddBookToShelf}
+              />
+            </div>
+          </>
+        )
+        
       }
-      footer={
+
+      footer={addBookToShelf ? <>
+       <div className="card-footer m-2 d-flex align-items-center justify-content-between">
+              <span className="text-muted" role="button" > &lt; back</span>
+              <div>
+                <button className="btn btn-dark me-md-3 float-end" type="button" onClick={handleClose}>Cancel</button>
+                <button className="btn btn-primary me-md-3 float-end" type="button">Add</button>
+              </div>
+            </div>
+      </>
+      :
         <>
-          <button class="btn btn-dark me-md-3 float-end" type="button" onClick={handleClose}>Cancel</button>
+          <button
+            class="btn btn-dark me-md-3 float-end"
+            type="button"
+            onClick={handleClose}
+          >
+            Cancel
+          </button>
+
         </>
       }
     />
