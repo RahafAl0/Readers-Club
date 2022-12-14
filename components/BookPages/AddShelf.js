ce123a44338af228a3c8eb128c41e1ff6c2bc4b2
RahafAl0 <ra.al9760@gmail.com>
2022-12-14 22:16:00 +0300

    remove comments

--- components/BookPages/AddShelf.js
+++ components/BookPages/AddShelf.js
@@ -29,22 +29,6 @@ const AddShelf = ({ handleClose, show }) => {
       }
     />
   );
-  // return (
-  //   <div class="m-auto w-400px h-225px">
-  //     <h5 className="p-5 text-center">Add Shelf</h5>
-  //     <input
-  //       className=" m-auto w-375px form-control"
-  //       type="text"
-  //       placeholder="Add shelf"
-  //       aria-label="default input example"
-  //     />
-  //     <div className="p-5">
-  //       <button class="btn btn-primary me-md-10 float-end" type="button">
-  //         Add
-  //       </button>
-  //     </div>
-  //   </div>
-  // );
 };
 
 export default AddShelf;
