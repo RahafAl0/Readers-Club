850352bbcf1cd55cde72874036d22d2ebc817536
RahafAl0 <ra.al9760@gmail.com>
2022-12-14 22:14:22 +0300

    add setTimeout to delay addBook window

--- components/BookPages/AddNewBook.js
+++ components/BookPages/AddNewBook.js
@@ -6,11 +6,13 @@ const AddNewBook = ({ handleClose, show }) => {
   const [addBookToShelf, setAddBookToShelf] = useState(false);
   const [bookDeatails, setBookDetails] = useState({title : "", author : ""});
   useEffect(() => {
-    return function cleanup() {
-      setAddBookToShelf(false)
-      setBookDetails({title : "", author : ""})
-    } 
-  })
+    setTimeout(() =>{
+      if(!show && addBookToShelf){
+        setAddBookToShelf(false)
+        setBookDetails({title : "", author : ""})
+      }
+    }, 200)
+  }, [show])
 
   function handleAddBookToShelf(book) {
     setAddBookToShelf(true);
@@ -116,7 +118,7 @@ const AddNewBook = ({ handleClose, show }) => {
       :
         <>
           <button
-            class="btn btn-dark me-md-3 float-end"
+            className="btn btn-dark me-md-3 float-end"
             type="button"
             onClick={handleClose}
           >
@@ -127,32 +129,6 @@ const AddNewBook = ({ handleClose, show }) => {
       }
     />
   );
-  // return (
-  //   <div>
-  //     <div className="card m-auto justify-content w-1000px h-600px border border-primary ">
-  //       <div className="card-body">
-  //         <div className="justify-content-between">
-  //           <h5 className="card-header p-2 m-2">Add book</h5>
-  //           <div className="m-10">
-  //             <input
-  //               class="form-control me-2"
-  //               type="search"
-  //               placeholder="Goerge Orewell"
-  //               aria-label="Search"
-  //             />
-  //           </div>
-  //         </div>
-  //         <div className="row m-2 p-2">
-  //           <BookCard />
-  //           <BookCard />
-  //           <BookCard />
-  //           <BookCard />
-  //         </div>
-  //         <div className="card-footer m-10"></div>
-  //       </div>
-  //     </div>
-  //   </div>
-  // );
 };
 
 export default AddNewBook;
