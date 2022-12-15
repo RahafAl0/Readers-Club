7e72cdba28f5fb098d4cd2b8908a9213aa0e1916
RahafAl0 <ra.al9760@gmail.com>
2022-12-15 19:02:10 +0300

    add json placeholder

--- pages/book/[bookName]/index.js
+++ pages/book/[bookName]/index.js
@@ -1,9 +1,20 @@
 import BookPage from "../../../components/BookPages/BookPage";
 
 
-export default function Book() {
-    return (
+export default function Book({todo}) {
+  console.log("response at client",todo)  
+  return (
       <BookPage />
-        
+      
     )}
 
+export const getServerSideProps = async(context) => {
+  const response =  await fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => response.json())
+  console.log("response at server =>",response)
+  return {
+    props : {
+      todo : response
+    }
+  }
+}
