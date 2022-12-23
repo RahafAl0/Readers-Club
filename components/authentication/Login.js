136ead999752e605d6d915035cb2a273dc787185
RahafAl0 <ra.al9760@gmail.com>
2022-12-23 21:41:56 +0300

    add login and getuser fns, wire up login

--- components/authentication/Login.js
+++ components/authentication/Login.js
@@ -1,60 +1,127 @@
 import { useState } from "react";
 
-
+const API_URL = 'http://localhost:8000';
 
 const Login = () => {
-  const [email, setEmail] = useState('')
-const [password, setPassword] = useState('')
-const [rememberMe, setRememberMe] = useState(false)
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [rememberMe, setRememberMe] = useState(false);
+
+  
+
+  async function login() {
+    try {
+        const response = await fetch(`${API_URL}/api/auth/login`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ username, password })
+        });
+        const data = await response.json();
+        console.log('login data is here => ',data);
+
+        localStorage.setItem('user', JSON.stringify({...data, username}));
+        await getUser();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+  async function getUser() {
+    const user = JSON.parse(localStorage.getItem('user'));
+    if (!user?.token) {
+        alert('You are not logged in');
+        return;
+    }
+    try {
+      const response = await fetch(`${API_URL}/api/auth/me`, {
+          headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `Token ${user.token}`
+          }
+      })
+       return response.json();
+  } catch (error) {
+      console.log(error);
+  }
+}
+  
 
   return (
     <form>
- 
-  <div className="form-outline mb-4">
-    <input type="email" id="email" value={email} className="form-control" onChange={(event) => {
-      setEmail(event.target.value)
-    }}/>
-    <label className="form-label" for="email">Email address</label>
-  </div>
+      <div className="form-outline mb-4">
+        <input
+          type="username"
+          id="username"
+          value={username}
+          className="form-control"
+          onChange={(event) => {
+            setUsername(event.target.value);
+          }}
+        />
+        <label className="form-label" for="email">
+          Email address
+        </label>
+      </div> 
 
-
-  <div className="form-outline mb-4">
-    <input type="password" id="password" value={password} className="form-control" onChange={(event) => {
-      setPassword(event.target.value)}}/>
-    <label className="form-label" for="password">Password</label>
-  </div>
-
- 
-  <div className="row mb-4">
-    <div className="col d-flex justify-content-center">
-     
-      <div className="form-check">
-        <input className="form-check-input" type="checkbox" value={rememberMe} id="rememberMe" checked onChange={(event) => {
-      setRememberMe(event.target.value)}}/>
-        <label className="form-check-label" for="rememberMe"> Remember me </label>
+      <div className="form-outline mb-4">
+        <input
+          type="password"
+          id="password"
+          value={password}
+          className="form-control"
+          onChange={(event) => {
+            setPassword(event.target.value);
+          }}
+        />
+        <label className="form-label" for="password">
+          Password
+        </label>
       </div>
-    </div>
 
-    <div className="col">
-     
-      <a href="#!">Forgot password?</a>
-    </div>
-  </div>
+      <div className="row mb-4">
+        <div className="col d-flex justify-content-center">
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              value={rememberMe}
+              id="rememberMe"
+              checked
+              onChange={(event) => {
+                setRememberMe(event.target.value);
+              }}
+            />
+            <label className="form-check-label" for="rememberMe">
+              {" "}
+              Remember me{" "}
+            </label>
+          </div>
+        </div>
 
+        <div className="col">
+          <a href="#!">Forgot password?</a>
+        </div>
+      </div>
 
-  <button type="button" className="btn btn-primary btn-block mb-4" onClick={() => {
-    console.log(email,password,rememberMe)
-  }}>Sign in</button>
+      <button
+        type="button"
+        className="btn btn-primary btn-block mb-4"
+        onClick={() => {
+          login();
+        }}
+      >
+        Sign in
+      </button>
 
-
-  <div className="text-center">
-    <p>Not a member? <a href="#!">Register</a></p>
-    
-
-   
-  </div>
-</form>
-  )
-}
+      <div className="text-center">
+        <p>
+          Not a member? <a href="#!">Register</a>
+        </p>
+      </div>
+    </form>
+  );
+};
 
 export default Login;
