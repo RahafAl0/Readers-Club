b8b19ab72b21fa3e53bdaf1920842824cebe51af
RahafAl0 <ra.al9760@gmail.com>
2022-12-23 23:04:22 +0300

    impl auth and redirect for protected route

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,28 @@
 import '../styles/globals.css'
 import Searchbar from '../components/BookPages/Searchbar'
 import Sidebar from '../components/Sidebar'
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
 
 function MyApp({ Component, pageProps }) {
+  const [user, setUser] = useState({})
+
+  const router = useRouter()
+  const isPublicRoute = router.route == '/auth/login' || router.route == '/auth/register';
+
+  useEffect(() => {
+    console.log(router);
+    if(isPublicRoute) return;
+    const userItem = JSON.parse(localStorage.getItem('user') || null);
+    if(!userItem || user.expiry < Date.now()) {
+      router.push('/auth/login');
+      return;
+    }
+    setUser(userItem)
+  }, [])
+
+  if(!isPublicRoute && !Object.keys(user).length) return <h1>Loading...</h1>
+
   return (
     <div className="d-flex flex-column flex-root">
       <div className='page d-flex flex-row flex-column-fluid'>
