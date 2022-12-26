eac3c1aaf50154fc1df2940babc70bc6eae6e16f
RahafAl0 <ra.al9760@gmail.com>
2022-12-26 23:29:49 +0300

    remove unnecessary import

---
+++ components/Searchbar.js
@@ -0,0 +1,14 @@
+const Searchbar = () => {
+  return(
+
+        <div className="container-fluid">
+          <form className="d-flex" role="search">
+            <input className="form-control me-5 w-800px" type="search" placeholder="Search" aria-label="Search"/>
+            <button className="btn btn-outline-success" type="submit">Search</button>
+          </form>
+        </div>
+
+  )
+}
+
+export default Searchbar;
