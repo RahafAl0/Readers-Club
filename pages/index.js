41701c13f03d3618be60f3616a6cedf57f1dedac
RahafAl0 <ra.al9760@gmail.com>
2022-12-28 22:17:10 +0300

    apply layout

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,25 @@
 import BookShelfRow from "../components/BookShelfRow";
+import Layout from "../components/Layout";
 
 export default function Home() {
   return (
-    <div>
-      {[
-        { title: "Currently Reading", shelfUrl: "currently-reading" },
-        { title: "Read", shelfUrl: "read" },
-        { title: "To Read", shelfUrl: "to-read" },
-        { title: "Want to read", shelfUrl: "want-to-read" },
-      ].map((val, index) => {
-        return (
-          <BookShelfRow
-            key={val.title}
-            title={val.title}
-            shelfUrl={val.shelfUrl}
-          />
-        );
-      })}
-    </div>
+    <Layout>
+      <div>
+        {[
+          { title: "Currently Reading", shelfUrl: "currently-reading" },
+          { title: "Read", shelfUrl: "read" },
+          { title: "To Read", shelfUrl: "to-read" },
+          { title: "Want to read", shelfUrl: "want-to-read" },
+        ].map((val, index) => {
+          return (
+            <BookShelfRow
+              key={val.title}
+              title={val.title}
+              shelfUrl={val.shelfUrl}
+            />
+          );
+        })}
+      </div>
+    </Layout>
   );
 }
