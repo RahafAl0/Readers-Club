b0ba5ec42be87801136f7c2b3740218117d30908
RahafAl0 <ra.al9760@gmail.com>
2022-12-30 23:25:11 +0300

    fix hardcoded token, format

--- components/BookPages/AddNewBook.js
+++ components/BookPages/AddNewBook.js
@@ -6,19 +6,21 @@ import { API_URL } from "../../utils/api";
 
 const AddNewBook = ({ handleClose, show }) => {
   const [addBookToShelf, setAddBookToShelf] = useState(false);
-  const [bookDeatails, setBookDetails] = useState({title : "", author : ""});
+  const [bookDeatails, setBookDetails] = useState({ title: "", author: "" });
 
-  const { isLoading, error, data } = useQuery('mostRecentBooks', () =>
-  fetch(`${API_URL}/api/books?limit=4`, {headers: {Authorization:'Token de631610d083b044e46a99ea94a90d548aa44daa7e796030673895564fdd75a8'}}).then(res =>
-    res.json()
-  )
-)
-console.log(data)
-  function handleDialogClose(){
-    setTimeout(() =>{
-      setAddBookToShelf(false)
-      setBookDetails({title : "", author : ""})   
-    }, 200)
+  const { isLoading, error, data } = useQuery("mostRecentBooks", () => {
+    const user = JSON.parse(localStorage.getItem("user") || null);
+    return fetch(`${API_URL}/api/books?limit=4`, {
+      headers: { Authorization: `Token ${user?.token}` },
+    }).then((res) => res.json());
+  });
+
+  console.log(data);
+  function handleDialogClose() {
+    setTimeout(() => {
+      setAddBookToShelf(false);
+      setBookDetails({ title: "", author: "" });
+    }, 200);
   }
 
   function handleAddBookToShelf(book) {
@@ -26,21 +28,19 @@ console.log(data)
     setBookDetails(book);
   }
 
-  if(isLoading){
-    return <Dialog 
-    show={show}
-    handleClose={() => {
-      handleClose();
-      handleDialogClose();
-    }}
-    header={
-      <h5>Add book</h5>
-    }
-    body={<h1>Loading...</h1>}
-
-    footer={<></>}
-  />
-    
+  if (isLoading) {
+    return (
+      <Dialog
+        show={show}
+        handleClose={() => {
+          handleClose();
+          handleDialogClose();
+        }}
+        header={<h5>Add book</h5>}
+        body={<h1>Loading...</h1>}
+        footer={<></>}
+      />
+    );
   }
 
   return (
@@ -52,17 +52,16 @@ console.log(data)
       }}
       header={
         <>
-            <h5>Add book</h5>
-            
-            <div className="m-10">
-              <input
-                className="form-control me-2"
-                type="search"
-                placeholder="Goerge Orewell"
-                aria-label="Search"
-              />
-            </div>
-          
+          <h5>Add book</h5>
+
+          <div className="m-10">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Goerge Orewell"
+              aria-label="Search"
+            />
+          </div>
         </>
       }
       body={
@@ -98,8 +97,7 @@ console.log(data)
                 Shelf4
               </button>
             </div>
-          </> 
-
+          </>
         ) : (
           <>
             <div className="row m-2 p-2">
@@ -149,35 +147,49 @@ console.log(data)
             </div>
           </>
         )
-        
       }
-
-      footer={addBookToShelf ? <>
-       <div className="card-footer m-2 d-flex align-items-center justify-content-between">
-              <span className="text-muted padding-left" role="button" > &lt; back</span>
+      footer={
+        addBookToShelf ? (
+          <>
+            <div className="card-footer m-2 d-flex align-items-center justify-content-between">
+              <span className="text-muted padding-left" role="button">
+                {" "}
+                &lt; back
+              </span>
               <div>
-                <button className="btn btn-dark me-md-3 float-end" type="button" onClick={() => {
-                  handleClose();
-                  handleDialogClose();
-                }}>Cancel</button>
-                <button className="btn btn-primary me-md-3 float-end" type="button">Add</button>
+                <button
+                  className="btn btn-dark me-md-3 float-end"
+                  type="button"
+                  onClick={() => {
+                    handleClose();
+                    handleDialogClose();
+                  }}
+                >
+                  Cancel
+                </button>
+                <button
+                  className="btn btn-primary me-md-3 float-end"
+                  type="button"
+                >
+                  Add
+                </button>
               </div>
             </div>
-      </>
-      :
-        <>
-          <button
-            className="btn btn-dark me-md-3 float-end"
-            type="button"
-            onClick={() => {
-              handleClose();
-              handleDialogClose();
-            }}
-          >
-            Cancel
-          </button>
-
-        </>
+          </>
+        ) : (
+          <>
+            <button
+              className="btn btn-dark me-md-3 float-end"
+              type="button"
+              onClick={() => {
+                handleClose();
+                handleDialogClose();
+              }}
+            >
+              Cancel
+            </button>
+          </>
+        )
       }
     />
   );
