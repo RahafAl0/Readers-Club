d91673d4c4f994af2a55ec2b8d8bf43d7cc82ab5
RahafAl0 <ra.al9760@gmail.com>
2022-12-30 23:27:47 +0300

    fix userItem bug, fix timestamp check

--- pages/_app.js
+++ pages/_app.js
@@ -1,45 +1,47 @@
-import '../styles/globals.css'
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
-import { QueryClient, QueryClientProvider } from 'react-query'
+import "../styles/globals.css";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { QueryClient, QueryClientProvider } from "react-query";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }) {
-  const [user, setUser] = useState({})
+  const [user, setUser] = useState({});
 
-  const router = useRouter()
-  const isPublicRoute = router.route == '/auth/login' || router.route == '/auth/register';
+  const router = useRouter();
+  const isPublicRoute =
+    router.route == "/auth/login" || router.route == "/auth/register";
   const isObjectEmpty = (obj) => {
-    if(typeof obj == "object" && obj !== null ) {
+    if (typeof obj == "object" && obj !== null) {
       return !Boolean(Object.keys(obj).length);
     } else return true;
+  };
 
-  } 
-
-  useEffect(() => {  
-    const userItem = JSON.parse(localStorage.getItem('user') || null);
-    if(isPublicRoute) { 
-      !isObjectEmpty(userItem) && router.push('/');
+  useEffect(() => {
+    const userItem = JSON.parse(localStorage.getItem("user") || null);
+    if (isPublicRoute) {
+      !isObjectEmpty(userItem) && router.push("/");
       return;
     }
-    if(isObjectEmpty(userItem) ||  user.expiry < Date.now()) {
-      router.push('/auth/login');
+
+    if (
+      isObjectEmpty(userItem) ||
+      new Date(userItem.expiry).getTime() < Date.now()
+    ) {
+      router.push("/auth/login");
       return;
     }
-    setUser(userItem)
-  }, [])
 
-  if(!isPublicRoute && isObjectEmpty(user)) return <h1>Loading...</h1>
+    setUser(userItem);
+  }, []);
+
+  if (!isPublicRoute && isObjectEmpty(user)) return <h1>Loading...</h1>;
 
   return (
     <QueryClientProvider client={queryClient}>
       <Component {...pageProps} />
     </QueryClientProvider>
-
- 
-  )
+  );
 }
 
-
-export default MyApp
+export default MyApp;
