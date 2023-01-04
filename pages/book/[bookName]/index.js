87da4e245e756626b041fdea7228ec0c8be938c0
RahafAl0 <ra.al9760@gmail.com>
2023-01-04 16:03:23 +0300

    remove unnecessary code

--- pages/book/[bookName]/index.js
+++ pages/book/[bookName]/index.js
@@ -1,20 +1,8 @@
 import BookPage from "../../../components/BookPages/BookPage";
 
 
-export default function Book({todo}) {
-  console.log("response at client",todo)  
+export default function Book() {  
   return (
       <BookPage />
       
     )}
-
-export const getServerSideProps = async(context) => {
-  const response =  await fetch('https://jsonplaceholder.typicode.com/todos/1')
-  .then(response => response.json())
-  console.log("response at server =>",response)
-  return {
-    props : {
-      todo : response
-    }
-  }
-}
