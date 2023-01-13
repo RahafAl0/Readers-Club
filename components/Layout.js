ce445b93c171f284a0e6490cc281af9291c0ed43
RahafAl0 <ra.al9760@gmail.com>
2023-01-13 21:20:04 +0300

    add arabic language

--- components/Layout.js
+++ components/Layout.js
@@ -1,17 +1,24 @@
 import Sidebar from "./Sidebar";
 import Searchbar from "./Searchbar";
+import LocaleSwitcher from "./LanguageSwitcher";
+import { Router, useRouter } from "next/router";
 
 export default function Layout({ children }) {
+  const router = useRouter()
+
   return(
   <div className="d-flex flex-column flex-root">
     <div className="page d-flex flex-row flex-column-fluid">
+      {router.locale == 'ar'?  <div className='sidebar bg-info w-325px'>   <Sidebar/>          </div> : null}
       <div className="wrapper d-flex flex-column flex-row-fluid">
-        <div className='header'> <Searchbar/>          </div>
+        <div className='header'>
+          <LocaleSwitcher />
+           <Searchbar/>          </div>
         <div className="d-flex flex-column flex-column-fluid">
           <main>{children}</main>
         </div>
       </div>
-      <div className='sidebar bg-info w-325px'>   <Sidebar/>          </div>
+      {router.locale == 'en'?  <div className='sidebar bg-info w-325px'>   <Sidebar/>          </div> : null}
     </div>
   </div>
   )
