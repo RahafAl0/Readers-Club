0cc633e3e932059291c8db05b2d7c88e9cb44571
RahafAl0 <ra.al9760@gmail.com>
2023-01-15 19:47:41 +0300

    edit buttons in book details

--- components/BookPages/BookDetail.js
+++ components/BookPages/BookDetail.js
@@ -1,66 +1,112 @@
 import { useRouter } from "next/router";
 import { useQuery } from "react-query";
-import { useTranslation } from 'next-i18next'
+import { useTranslation } from "next-i18next";
 import getAxiosInstance from "../../utils/api/getAxiosInstance";
 import Layout from "../Layout";
 
 const BookDetail = () => {
+  const { t } = useTranslation("common");
 
-  const { t } = useTranslation('common');
-  
+  const {
+    query: { bookId },
+  } = useRouter();
 
-  const {query: {bookId} } = useRouter()
-
-  const { isLoading, error, data } = useQuery("bookDetails", () => {
-    return getAxiosInstance({ auth: true })
+  const { isLoading, error, data } = useQuery("bookDetails", async () => {
+    const book = await getAxiosInstance({ auth: true })
       .get(`/books/${bookId}`)
       .then((res) => res.data);
+
+    let userBook = null;
+
+    if (book.user_book_id) {
+      userBook = await getAxiosInstance({ auth: true })
+        .get(`/user/books/${book.user_book_id}`)
+        .then((res) => res.data);
+      return { book, userBook };
+    } else return { book, userBook: null };
   });
+
+  const { book, userBook } = data || {};
+
+  const renderUserBooksButtons = () => {
+    const {is_currently_reading} = userBook || {}
+    return !userBook ? null : (
+      <>
+        {!is_currently_reading ?   <button
+            type="button"
+            className="btn btn-primary"
+            onClick={async (event) => {
+              await getAxiosInstance({ auth: true }).put(
+                `/user/currently-reading`,{user_book_id: userBook.id, status: true}
+              );
+              alert("done add book to currently reading books");
+            }}
+          >
+            {t("markAsCurrentlyReading")}
+          </button> : (
+          <button
+            type="button"
+            className="btn btn-primary"
+            onClick={async (event) => {
+              await getAxiosInstance({ auth: true }).put(
+                `/user/books/${book.user_book_id}/done`
+              );
+              alert("done add book to finished books");
+            }}
+          >
+            {t("markAsRead")}
+          </button>
+        )}
+        
+        <button type="button" className="btn btn-primary">
+          {t("removeBook")}
+        </button>
+      </>
+    );
+  };
 
   return (
     <Layout>
-    
-      {
-        isLoading ? <h1>Loading your book detail</h1> : <div className="card border mb-3 w-700px h-120px m-auto">
-        <div className="row g-0">
-          <div className="col-md-4">
-            <img
-              src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1327144697l/3744438.jpg"
-              className="img-fluid rounded-start"
-              alt="..."
-              style={{ height: "450px" }}
-            />
-          </div>
-          <div className="col-md-6 ">
-            <div className="card-header">
-              <p className="fw-bold fs-1">{data.title}</p>
-              <p className="fs-1 fw-bold">{data.authors}</p>
+      {isLoading ? (
+        <h1>Loading your book detail</h1>
+      ) : (
+        <div className="card border mb-3 w-700px h-120px m-auto">
+          <div className="row g-0">
+            <div className="col-md-4">
+              <img
+                src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1327144697l/3744438.jpg"
+                className="img-fluid rounded-start"
+                alt="..."
+                style={{ height: "450px" }}
+              />
             </div>
-            <div className="card-body">
-              <p className="card-text">
-                {data.description}
-              </p>
-              <div className="card-footer">
-                <p>{data.pages} pages</p>
-                <p>Published {data.publication_date} by ({data.publisher})</p>
+            <div className="col-md-6 ">
+              <div className="card-header">
+                <p className="fw-bold fs-1">{book.title}</p>
+                <p className="fs-1 fw-bold">{book.authors}</p>
+              </div>
+              <div className="card-body">
+                <p className="card-text">{book.description}</p>
+                <div className="card-footer">
+                  <p>{book.pages} pages</p>
+                  <p>
+                    Published {book.publication_date} by ({book.publisher})
+                  </p>
+                </div>
               </div>
             </div>
+            <div className="col-md-2">
+              {renderUserBooksButtons()}
+              {/* <button type="button" className="btn btn-primary">
+                {t("addToShelf")}
+              </button> */}
+              <button type="button" className="btn btn-primary">
+                {t("addBook")}
+              </button>
+            </div>
           </div>
-          <div className="col-md-2" >
-          <button type="button" className="btn btn-primary">
-          {t("markAsRead")}
-        </button>
-        <button type="button" className="btn btn-primary">
-          {t("marsAsDone")}
-        </button>
-        <button type="button" className="btn btn-primary">
-          {t("addToShelf")}
-        </button>
-          </div>
-
         </div>
-      </div>
-      }
+      )}
     </Layout>
   );
 };
